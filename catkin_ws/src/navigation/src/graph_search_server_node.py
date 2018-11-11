f550c2eace782beb7598708fce04b2536432822f
moyn8 <nicholas.moy5@gmail.com>
2018-11-11 15:09:08 -0500

    fixed node print statement again

--- catkin_ws/src/navigation/src/graph_search_server_node.py
+++ catkin_ws/src/navigation/src/graph_search_server_node.py
@@ -37,8 +37,8 @@ class graph_search_server():
         self.image_pub.publish(self.bridge.cv2_to_imgmsg(overlay, "bgr8"))
 
     def handle_graph_search(self,req):
-	    # Checking if nodes exists
-			  print self.duckietown_graph._nodes
+	# Checking if nodes exists
+	print self.duckietown_graph._nodes
         if (req.source_node not in self.duckietown_graph) or (req.target_node not in self.duckietown_graph):
             print "Source or target node do not exist."
             self.publishImage(req, [])
