68435a781dd00c2bb38a6f0b57d6b748f0dbf328
Jackie <scanlj2@rpi.edu>
2018-11-23 11:50:12 -0500

    The publisher and subscriber work now. run camera_ros_python to output a video stream (from file) to the topic, or alternately the pi camera node. run aruco_ros_python to see the output. simple_cv_test.py does the same thing but not in the ros framework. test_image was used to test cv, and no_ros_aruco_test is useless on ubuntu so i deleted it.

--- catkin_ws/src/aruco_tags/scripts/camera_ros_python.py
+++ catkin_ws/src/aruco_tags/scripts/camera_ros_python.py
@@ -7,26 +7,36 @@ from sensor_msgs.msg import CompressedImage
 import numpy as np
 import cv2, PIL
 from cv2 import aruco
+from cv_bridge import CvBridge
 
 def talker():
     pub = rospy.Publisher('/howard17/camera_node/image/compressed', CompressedImage, queue_size=10)
     rospy.init_node('aruco_talker')
     rate = rospy.Rate(10) # 10hz
-    cap = cv2.VideoCapture('/home/jackie/duckietown/catkin_ws/src/aruco_tags/scripts/test_videos/test1.mp4')
+    br = CvBridge()
+    cap = cv2.VideoCapture('test1.mp4')
+    #print('Got the cap')
     #cap = cv2.VideoCapture(0)
     while not rospy.is_shutdown():
         ret, frame = cap.read()
 
         if ret == True:
+	    
+            #print('ret is true')
             # Our operations on the frame come here
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
             # Display the resulting frame
-            cv2.imshow('frame',gray)
-
+            #cv2.imshow('frame',gray)
+            #if cv2.waitKey(1) & 0xFF == ord('q'):
+            #break
             #rospy.loginfo(hello_str)
-            #pub.publish(hello_str)
-        rate.sleep()
+
+	    # Convert the video to a ros Compressed image
+	    message = br.cv2_to_compressed_imgmsg(frame)
+            pub.publish(message)
+	    #rospy.loginfo(message)
+        #rate.sleep()
     # Upon shutdown
     cap.release()
     cv2.destroyAllWindows()
