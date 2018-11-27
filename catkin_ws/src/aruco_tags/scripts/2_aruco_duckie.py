772a637909d0d46541178ffe5a3aaeac5bec1c3b
Jackie <scanlj2@rpi.edu>
2018-11-27 18:00:44 -0500

    Got aruco_duckie.py ready for launching, made the markers a little smaller. Both detect at .4m away now. 2_aruco_duckie.py is the first implmenetation for publishing on two seperate topics for each tag - no timer bc none is necssary.

--- catkin_ws/src/aruco_tags/scripts/2_aruco_duckie.py
+++ catkin_ws/src/aruco_tags/scripts/2_aruco_duckie.py
@@ -10,7 +10,7 @@ from duckietown_msgs.msg import BoolStamped
 # Runs when tag 1 is detected (tag 1 = obstacle blocking lane)
 def pose(data):
 
-    global min_dist_lane, time_out, flag, pub_too_close
+    global min_dist_lane, pub_tag_1
     
     # Get the distance away from the duckiebot
     dis = data.position.z
@@ -29,18 +29,13 @@ def pose(data):
 	# Print a message
 	rospy.loginfo('Tag 1 is too close: %s m', dis)
 
-	# Publish to the too_close topic:
-	pub_too_close.publish(b)
-
-	# Reset the timer and tell timer to start counting
-        time_out = 0
-        flag = True
-
+	# Publish to the pub_tag_1 topic to be read by fsm state
+	pub_tag_1.publish(b)
 
 # Runs when tag 2 is detected (tag 2 = obstacle blocking road)
 def pose2(data):
 
-    global min_dist_road, time_out, flag, pub_too_close
+    global min_dist_road, pub_tag_2
 
     # Get the distance away from the duckiebot
     dis = data.position.z
@@ -59,44 +54,10 @@ def pose2(data):
 	# Print a message
 	rospy.loginfo('Tag 2 is too close: %s m', dis)
 
-	# Publish to the too_close topic
-	pub_too_close.publish(b)
+	# Publish to the pub_tag_2 topic to be read by fsm state
+	pub_tag_2.publish(b)
 
-	# Reset the timer and tell timer to start counting
-	time_out = 0
-	flag = True
-
-# Sends a false after the tag has been gone for 5 seconds
-def timer():
-
-    global time_out, flag, pub_too_close
-
-    b = BoolStamped()
-    b.data = False
-
-    # Essentially a while(true) loop
-    while not rospy.is_shutdown():
-
-        # Sleep 1 second
-        rospy.sleep(1)
-
-        # If the last message published was true
-        if flag == True:
-
-            # Increase the time
-            time_out+=1
-            rospy.loginfo('Incrementing time out!')
-
-            # If no message has been published in 5 seconds
-            # publish "false" to the topic "too close"
-        if time_out >= 5:
-            pub_too_close.publish(b)
-            time_out = 0
-            flag = False
-            rospy.loginfo('Timed out!')
-
-
-def listener():
+def  listener():
 
     # Initialize the node
     rospy.init_node('aruco_duckie')
@@ -107,28 +68,15 @@ def listener():
     global min_dist_road
     min_dist_road = .4
 
-    # Keeps track of how many seconds have gone by since
-    # we last heard a published message
-    global time_out
-    time_out = 0
+    # Set up the publishers
+    global pub_tag_2 pub_tag_1
+    pub_tag_1 = rospy.Publisher('/howard17/obstacle_safety_node/pub_tag_1', BoolStamped, queue_size=1)
+    pub_tag_2 = rospy.Publisher('/howard17/obstacle_safety_node/pub_tag_2', BoolStamped, queue_size=1)
 
-    # Keeps track of if we even need to be tracking it
-    global flag
-    flag = True
-    
-    
-    # Set up the publisher
-    # Currently doesn't account for the difference between tags
-    global pub_too_close
-    pub_too_close = rospy.Publisher('/howard17/obstacle_safety_node/object_too_close', BoolStamped, queue_size=1)
-    
     # Subscribe to the nodes that give poses of tags
     rospy.Subscriber('/aruco_double/pose', Pose, pose)
     rospy.Subscriber('/aruco_double/pose2', Pose, pose2)
  
-    # Starting timing things
-    timer()
-
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
